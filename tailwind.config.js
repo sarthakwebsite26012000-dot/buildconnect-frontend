8da491387954a56fc7ffbf3a29f617bcf785e330
sarthakwebsite26012000-dot <sarthakwebsite26012000@gmail.com>
2025-12-09 19:30:51 +0530

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#1E40AF',
+        secondary: '#F97316',
+        accent: '#0EA5E9',
+      },
+    },
+  },
+  plugins: [],
+}
